289a1848ba30dc3cfb1f13221641476325d9b3c1
Lokman Demirci <lokmandemirci@gmail.com>
2022-02-02 10:14:37 +0300

    first trigger test

--- main.go
+++ main.go
@@ -10,7 +10,7 @@ func main() {
 	fmt.Println("Go Docker Tutorial")
 
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Hello Docker from Jenkins on ArgoCD...")
+		fmt.Fprintf(w, "Hello Docker from Jenkins on ArgoCD --- Lokman editliyor ...")
 	})
 
 	log.Fatal(http.ListenAndServe(":8081", nil))
